582d330e0a3bf82730bda309fa7b9861057bd877
EmilieGDU <emilie.gendreau@yahoo.fr>
2022-06-05 17:15:48 +0200

    BACKEND : Implémentation des routes PUT

--- backend/app.js
+++ backend/app.js
@@ -76,70 +76,133 @@ app.use(express.json());
 // CREATE
 app.post("/api/users", (req, res, next) => {
     User.create(req.body)
-        .then((user) => {
-            const message = `L'utilisateur '${ req.body.firstName } ${ req.body.lastName }' a été enregistré.`;
-            res.status(201).json({ message, data: user });
-        })
-        .catch((error) => res.status(400).json({ error }));
+    .then((user) => {
+        const message = `L'utilisateur '${ user.firstName } ${ user.lastName }' a été enregistré.`;
+        res.status(201).json({ message, data: user });
+    })
+    .catch((error) => res.status(400).json({ error }));
 });
 
 app.post("/api/posts", (req, res, next) => {
     Post.create(req.body)
-        .then((post) => {
-            const message = `Le post intitulé '${ req.body.title }' a été enregistré.`;
-            res.status(201).json({ message, data: post });
-        })
-        .catch((error) => res.status(400).json({ error }));
+    .then((post) => {
+        const message = `Le post intitulé '${ post.title }' a été enregistré.`;
+        res.status(201).json({ message, data: post });
+    })
+    .catch((error) => res.status(400).json({ error }));
 });
 
 app.post("/api/posts/:id/comments", (req, res, next) => {
     Comment.create(req.body)
-        .then(() => res.status(201).json({ message: "Commentaire enregistré." }))
-        .catch((error) => res.status(400).json({ error }));
+    .then(() => res.status(201).json({ message: "Commentaire enregistré." }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 // READ
 app.get("/api/posts", (req, res, next) => {
     Post.findAll()
-        .then((posts) => {
-            const message = "L'ensemble des posts a été récupéré.";
-            res.status(200).json({ message, data: posts });
-        })
-        .catch((error) => res.status(400).json({ error }));
+    .then((posts) => {
+        const message = "L'ensemble des posts a été récupéré.";
+        res.status(200).json({ message, data: posts });
+    })
+    .catch((error) => res.status(404).json({ error }));
 });
 
 app.get("/api/posts/:id", (req, res, next) => {
     Post.findByPk(req.params.id)
-        .then((post) => {
-            if (post === null) {
-                const message = "Post non trouvé.";
-                res.status(400).json({ message });
-            } else {
-                const message = "Un post a été récupéré.";
-                res.status(200).json({ message, data: post });
-            };
-        })
-        .catch((error) => res.status(400).json({ error }));
+    .then((post) => {
+        if (post === null) {
+            const message = "Post non trouvé.";
+            res.status(404).json({ message });
+        } 
+        else {
+            const message = "Un post a été récupéré.";
+            res.status(200).json({ message, data: post });
+        };
+    })
+    .catch((error) => res.status(404).json({ error }));
 });
 
 app.get("/api/posts/:id/comments", (req, res, next) => {
     Comment.findAll({ where: { PostId: req.params.id} })
-        .then((comments) => {
-            if (comments.length === 0) {
-                const message = "Post non trouvé.";
-                res.status(400).json({ message });
-            } else {
-                const message = "L'ensemble des commentaires a été récupéré.";
-                res.status(200).json({ message, data: comments });
-            }
-        })
-        .catch((error) => res.status(400).json({ error }));
+    .then((comments) => {
+        if (comments.length === 0) {
+            const message = "Post non trouvé.";
+            res.status(404).json({ message });
+        } 
+        else {
+            const message = "L'ensemble des commentaires a été récupéré.";
+            res.status(200).json({ message, data: comments });
+        }
+    })
+    .catch((error) => res.status(404).json({ error }));
 });
 
 // UPDATE
 
+app.put("/api/users/:id", (req,res, next) => {
+    const userId = req.params.id;
+    User.findByPk(userId)
+    .then((user) => {
+        if (user === null) {
+            const message = "Utilisateur non trouvé.";
+            res.status(404).json({ message });
+        } 
+        else {
+            User.update(req.body, { where: {id: userId} })
+            .then(() => {
+                User.findByPk(userId)
+                .then((updatedUser) => {
+                    const message = `L'utilisateur '${ updatedUser.firstName } ${ updatedUser.lastName }' a été modifié.`;
+                    res.status(200).json({ message, data: updatedUser });
+                })
+            })
+            .catch((error) => res.status(400).json({ error })); 
+        };
+    })
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/posts/:id", (req,res, next) => {
+    const postId = req.params.id;
+    Post.findByPk(postId)
+    .then((post) => {
+        if (post === null) {
+            const message = "Post non trouvé.";
+            res.status(404).json({ message });
+        } 
+        else {
+            Post.update(req.body, { where: {id: postId} })
+            .then(() => {
+                Post.findByPk(postId)
+                .then((updatedPost) => {
+                    const message = `Le post intitulé '${ updatedPost.title }' a été modifié.`;
+                    res.status(200).json({ message, data: updatedPost });
+                })
+            })
+            .catch((error) => res.status(400).json({ error })); 
+        };
+    })
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/posts/:id/comments/:id", (req,res, next) => {
+
+});
+
 // DELETE
 
+app.delete("/api/users/:id", (req,res, next) => {
+
+});
+
+app.delete("/api/posts/:id", (req,res, next) => {
+
+});
+
+app.delete("/api/comments/:id", (req,res, next) => {
+
+});
 
 
 module.exports = app;
