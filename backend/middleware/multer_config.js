730849e72659d6705f6c942b122a7cc02c68ca50
EmilieGDU <emilie.gendreau@yahoo.fr>
2022-06-18 17:07:57 +0200

    BACKEND : Configuration du middleware de gestion des fichiers entrants avec Multer

---
+++ backend/middleware/multer_config.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+
+// "Dictionary" to generate the appropriate file extension 
+const MIME_TYPES = {
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
+    "image/png": "png"
+};
+
+// Configuring disk storage path and filename for incoming files
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "images");
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(" ").join("_").split(".")[0];
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + "." + extension);
+    }
+});
+
+// Exporting the package, configured to only accept single files
+module.exports = multer({ storage: storage }).single("image");
